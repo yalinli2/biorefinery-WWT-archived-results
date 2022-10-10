fa8ced972c05ed6b73b63e3e5f336ae52eb171ad
Yalin Li <mailto.yalin.li@gmail.com>
2022-10-10 10:56:37 -0500

    add generic console scripts for easy baseline results retrieval

---
+++ console_scripts.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 10 10:40:37 2022
+
+@author: yalinli_cabbi
+"""
+
+from biorefineries.wwt.corn import create_cn_comparison_models as f_cn
+from biorefineries.wwt.sugarcane1g import create_sc1g_comparison_models as f_sc1g
+from biorefineries.wwt.oilcane1g import create_oc1g_comparison_models as f_oc1g
+from biorefineries.wwt.cornstover import create_cs_comparison_models as f_cs
+from biorefineries.wwt.sugarcane2g import create_sc2g_comparison_models as f_sc2g
+from biorefineries.wwt.oilcane2g import create_oc2g_comparison_models as f_oc2g
+from biorefineries.wwt.lactic import create_la_comparison_models as f_la
+
+f_dct = {
+    'cn': f_cn,
+    'sc1g': f_sc1g,
+    'oc1g': f_oc1g,
+    'cs': f_cs,
+    'sc2g': f_sc2g,
+    'oc2g': f_oc2g,
+    'la': f_la,
+    }
+
+modules_1g = ['cn', 'sc1g', 'oc1g']
+modules_2g = ['sc2g', 'oc2g', 'la']
+modules_all = modules_1g + modules_2g
+
+def load(module):
+    f = f_dct[module]
+    global exist_model, new_model, exist_sys, new_sys, \
+            exist_u, new_u, exist_s, new_s, exist_tea, new_tea
+    exist_model, new_model = f()
+    exist_sys, new_sys = exist_model.system, new_model.system
+    exist_f, new_f = exist_sys.flowsheet, new_sys.flowsheet
+    exist_u, new_u = exist_f.unit, new_f.unit
+    exist_s, new_s = exist_f.stream, new_f.stream
+    exist_tea, new_tea = exist_sys.TEA, new_sys.TEA
+
+if __name__ == '__main__':
+    module = 'cn'
+    # module = 'sc1g'
+    # module = 'oc1g'
+    # module = 'cs'
+    # module = 'sc2g'
+    # module = 'oc2g'
+    # module = 'la'
+    load(module)
